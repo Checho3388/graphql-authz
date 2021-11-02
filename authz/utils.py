22548b1a57e59815f199cff900c4525ce307190f
egrondona <egrondona@onapsis.com>
2021-11-02 07:30:23 -0300

    Remove unused code

--- authz/utils.py
+++ authz/utils.py
@@ -1,10 +1,5 @@
 from graphql import GraphQLResolveInfo
 
-CASBIN_CONTEXT_PATH_CACHE_KEY = '_casbinPathCache'
-CASBIN_CONTEXT_ENFORCER_KEY = '_casbinEnforcer'
-CASBIN_CONTEXT_ROLE_KEY = '_casbinRole'
-CASBIN_AUTH_TYPE_NAME = 'CasbinCanVisit'
-
 
 def info_to_path(info: GraphQLResolveInfo) -> str:
     """"Get the full path of current endpoint by GraphQLResolveInfo."""
